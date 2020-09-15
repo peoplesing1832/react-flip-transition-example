e7504f89531c1f04cf7853d32bdcad0f84b52127
zhangyue0219 <zhangyue0219@hupu.com>
2020-09-15 16:46:58 +0800

    feat: 添加矩阵的例子

--- src/page/Matrix/index.js
+++ src/page/Matrix/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
+import {
+  shuffle
+} from 'lodash';
+import {
+  TransitionFLIP,
+  TransitionFLIPS
+} from 'react-flip-transition';
+import './index.css';
 
 const Matrix = () => {
+
+  const [matrix, setMatrix] = useState(() => {
+    let arr = [];
+    for (let i = 0; i < 99; i++) {
+      arr.push(i);
+    }
+    return arr;
+  });
+
+  useEffect(() => {
+    setInterval(() => {
+      setMatrix((prev) => [...shuffle(prev)]);
+    }, 1500);
+  }, []);
+
   return (
-    <div>矩阵</div>
+    <div className="matrix-container">
+      <TransitionFLIPS
+        wrapClassName="matrix"
+        duration={900}
+        easing="cubic-bezier(.62,.78,.62,1.35)"
+      >
+        {
+          matrix && matrix.map((m) => (
+            <TransitionFLIP flipId={m} key={m}>
+              <div className="matrix-item">{ m }</div>
+            </TransitionFLIP>
+          ))
+        }
+      </TransitionFLIPS>
+    </div>
   )
 }
 
