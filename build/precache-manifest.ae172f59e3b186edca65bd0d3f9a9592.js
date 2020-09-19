132d5a3826c4bf6d7e943bdf6fb6432ff4ef57de
peoplesing1832 <peoplesing1832@gmail.com>
2020-09-19 23:48:57 +0800

    feat: 打包

---
+++ build/precache-manifest.ae172f59e3b186edca65bd0d3f9a9592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558fc080b4b360c4549b149512f4bdd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bec35103711cfea368",
+    "url": "/static/css/main.861a3343.chunk.css"
+  },
+  {
+    "revision": "9976f55f4d7d00e03c55",
+    "url": "/static/js/2.9f7a4068.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.9f7a4068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bec35103711cfea368",
+    "url": "/static/js/main.6e1f852f.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
