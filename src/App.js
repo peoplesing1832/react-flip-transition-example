2eb5e902d00c22ff18443de68e483ca26633be93
peoplesing1832 <peoplesing1832@gmail.com>
2021-01-14 08:47:21 +0800

    Update: 更新例子

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, {
+  useState,
+  useRef,
+} from 'react';
 import './App.css';
 import {
-  Transition
+  Transition,
+  Observer,
+  Transitions,
 } from 'react-flip-transition';
 
 function App() {
   const [state, setState] = useState(false);
   const [state2, setState2] = useState(false);
+  const [state3, setState3] = useState(false);
+  const [list, setList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const nextNum = useRef(10);
+
+  const randomIndex = () => {
+    return Math.floor(Math.random() * list.length);
+  };
 
   return (
     <div className="App">
@@ -22,7 +34,7 @@ function App() {
         animation={state}
         duration={{
           enter: 200,
-          leave: 400,
+          leave: 600,
         }}
       >
         <div>hello world</div>
@@ -42,6 +54,73 @@ function App() {
         <div>At this point I have a request for our fans. If any of you in any way hate homosexuals, people of a different color, or women, please do this one favor for us─leave us the fuck alone! Don't come to our shows, and don't buy our records.</div>
       </Transition>
       <h3 className="doc-title">多个元素过渡</h3>
+      <button
+        onClick={() => setState3(!state3)}
+        className="doc-button"
+      >
+        toggle
+      </button>
+      <Transitions
+        animation={true}
+        prefix="fade"
+      >
+        <Transition>
+          <div>1</div>
+        </Transition>
+        <Transition>
+          <div>2</div>
+        </Transition>
+        <Transition>
+          <div>3</div>
+        </Transition>
+        <Transition>
+          <div>4</div>
+        </Transition>
+        <Transition>
+          <div>5</div>
+        </Transition>
+        <Transition>
+          <div>6</div>
+        </Transition>
+      </Transitions>
+      <h3 className="doc-title">列表过渡</h3>
+      <button
+        className="doc-button"
+        onClick={() => {
+          list.splice(randomIndex(), 0, nextNum.current++)
+          setList([...list]);
+        }}
+      >
+        Add
+      </button>
+      <button
+        className="doc-button"
+        onClick={() => {
+          list.splice(randomIndex(), 1);
+          setList([...list]);
+        }}
+      >
+        Remove
+      </button>
+      <Observer
+        wrapClass="observer-list"
+        wrap="div"
+      >
+        {
+          list && list.map((item) => {
+            return (
+              <Transition
+                key={item}
+                name="observer"
+                duration={1000}
+              >
+                <div className="observer-list-item">{ item }</div>
+              </Transition>
+            )
+          })
+        }
+      </Observer>
+      <h3 className="doc-title">排序过渡</h3>
     </div>
   );
 }
