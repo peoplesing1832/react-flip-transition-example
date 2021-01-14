811a4c34b350f152187eb50ee345a36c949e0483
peoplesing1832 <68176628+peoplesing1832@users.noreply.github.com>
2021-01-14 11:36:19 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ import {
   Transition,
   Observer,
   Transitions,
+  Flip,
+  Flips,
 } from 'react-flip-transition';
+import { flip, shuffle } from 'lodash'
 
 function App() {
-  const [state, setState] = useState(false);
-  const [state2, setState2] = useState(false);
-  const [state3, setState3] = useState(false);
+  const [state, setState] = useState(true);
+  const [state2, setState2] = useState(true);
+  const [state3, setState3] = useState(true);
   const [list, setList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [list2, setList2] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [list3, setList3] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [list4, setList4] = useState(() => {
+    const result = [];
+    for (let i = 1; i <= 81; i++) {
+      result.push(i);
+    }
+    return result;
+  });
   const nextNum = useRef(10);
 
   const randomIndex = () => {
@@ -61,27 +73,30 @@ function App() {
         toggle
       </button>
       <Transitions
-        animation={true}
+        masterSwitch={state3}
         prefix="fade"
+        interval={300}
       >
-        <Transition>
-          <div>1</div>
-        </Transition>
-        <Transition>
-          <div>2</div>
-        </Transition>
-        <Transition>
-          <div>3</div>
-        </Transition>
-        <Transition>
-          <div>4</div>
-        </Transition>
-        <Transition>
-          <div>5</div>
-        </Transition>
-        <Transition>
-          <div>6</div>
-        </Transition>
+        <ul>
+          <Transition unmount>
+            <li className="flip1-list-item">1</li>
+          </Transition>
+          <Transition unmount>
+            <li className="flip1-list-item">2</li>
+          </Transition>
+          <Transition unmount>
+            <li className="flip1-list-item">3</li>
+          </Transition>
+          <Transition unmount>
+            <li className="flip1-list-item">4</li>
+          </Transition>
+          <Transition unmount>
+            <li className="flip1-list-item">5</li>
+          </Transition>
+          <Transition unmount>
+            <li className="flip1-list-item">6</li>
+          </Transition>
+        </ul>
       </Transitions>
       <h3 className="doc-title">列表过渡</h3>
       <button
@@ -121,6 +136,97 @@ function App() {
         }
       </Observer>
       <h3 className="doc-title">排序过渡</h3>
+      <button
+        className="doc-button"
+        onClick={() => {
+          setList2(shuffle(list2));
+        }}
+      >
+        Shuffle
+      </button>
+      <Flips
+        name="flip1"
+        wrap="ul"
+      >
+        {
+          list2 && list2.map((item) => {
+            return (
+              <Flip key={item}>
+                <li className="flip1-list-item">{ item }</li>
+              </Flip>
+            );
+          })
+        }
+      </Flips>
+      <button
+        className="doc-button"
+        onClick={() => {
+          setList3(shuffle(list3));
+        }}
+      >
+        Shuffle
+      </button>
+      <button
+        className="doc-button"
+        onClick={() => {
+          list3.splice(randomIndex(), 0, nextNum.current++)
+          setList3([...list3]);
+        }}
+      >
+        Add
+      </button>
+      <button
+        className="doc-button"
+        onClick={() => {
+          list3.splice(randomIndex(), 1);
+          setList3([...list3]);
+        }}
+      >
+        Remove
+      </button>
+      <Flips
+        wrapClass="observer-list"
+        wrap="div"
+        name="flip2"
+        inOutDuration={1000}
+      >
+        {
+          list3 && list3.map((item) => {
+            return (
+              <Flip
+                key={item}
+              >
+                <div className="observer-list-item">{ item }</div>
+              </Flip>
+            )
+          })
+        }
+      </Flips>
+      <button
+        className="doc-button"
+        onClick={() => {
+          setList4(shuffle(list4));
+        }}
+      >
+        Shuffle
+      </button>
+      <Flips
+        wrapClass="matrix"
+        wrap="div"
+        name="matrix"
+      >
+        {
+          list4 && list4.map((item) => {
+            return (
+              <Flip
+                key={item}
+              >
+                <div className="matrix-item">{ item }</div>
+              </Flip>
+            )
+          })
+        }
+      </Flips>
     </div>
   );
 }
